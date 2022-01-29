e1291282d8d0569b53c69aa702678463c3d1e83a
mas.ked.rin <mas.kedrin@gmail.com>
2022-01-29 10:47:00 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='YourNal',
+    version='0.0.01a',
+    packages=['orm'],
+    url='https://github.com/m45ked/YourNal',
+    license='',
+    author='michal',
+    author_email='mas.ked.rin@gmail.com',
+    description='',
+    install_requires=[
+        'flask',
+        'flask-wtf',
+        'flask-sqlalchemy',
+        'sqlalchemy'
+    ],
+)
